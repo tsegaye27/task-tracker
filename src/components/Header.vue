656ffa2004c46b48db4cd715d2483c01f9765b8c
tsegaye27 <ttalegn29@gmail.com>
2024-05-30 15:20:46 +0300

    creating a Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Button from "./Button.vue";
+defineProps<{ title?: string }>();
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" />
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  justify-content: space-between;
+}
+</style>
