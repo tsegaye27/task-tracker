39da34864d67a58c4ff714c745896a3b25094bc3
tsegaye27 <ttalegn29@gmail.com>
2024-05-30 16:47:14 +0300

    add initial data

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,49 @@
 <script setup lang="ts">
-defineProps<{ text?: string; color?: string }>();
+import { onMounted, ref } from "vue";
+defineProps<{
+  text?: string;
+  color?: string;
+}>();
+type TaskProps = {
+  id: number;
+  text: string;
+  day: string;
+  reminder: boolean;
+};
+const tasks = ref<TaskProps[]>([]);
+onMounted(() => {
+  tasks.value = [
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: "May 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "May 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "May 7th at 12:30pm",
+      reminder: false,
+    },
+  ];
+});
 </script>
 
 <template>
-  <button :style="{ background: color }" class="btn">{{ text }}</button>
+  <button :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
 </template>
 
 <style scoped>
 .btn {
-  background-color: rgb(86, 205, 86);
+  background-color: #56cd56;
   color: white;
   border: none;
   padding: 0.5rem 0.85rem;
