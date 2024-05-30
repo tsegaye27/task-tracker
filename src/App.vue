1ef9acf7b1b15489625dd5cb4bd9c39c0e949ec3
tsegaye27 <ttalegn29@gmail.com>
2024-05-30 15:21:13 +0300

    including Header component to App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Header title="Task Tracker" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding: 10px;
+  border: 2px solid #7474db;
+  border-radius: 0.65rem;
+}
+</style>
