1f2f72d6b2d114478ec6f31149fb0db080efa3d9
tsegaye27 <ttalegn29@gmail.com>
2024-05-31 11:44:39 +0300

    minor changes

---
+++ src/components/Tasks.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { TaskProps } from "@/types/TaskProps";
+import Task from "./Task.vue";
+defineProps({
+  tasks: {
+    type: Array as () => TaskProps[],
+    required: true,
+  },
+  onDelete: {
+    type: Function as unknown as () => (id: number) => void,
+    required: true,
+  },
+  toggleReminder: {
+    type: Function as unknown as () => (id: number) => void,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div v-for="task in tasks">
+    <Task
+      :task="task as TaskProps"
+      :onDelete="onDelete"
+      :toggleReminder="toggleReminder"
+    />
+  </div>
+</template>
+
+<style></style>
